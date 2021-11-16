1902d31a325335c6d67da77dd7cf906c73249b5d
Pitimonster <piotrszym159@gmail.com>
2021-11-16 22:53:00 +0100

    Revert "Revert "CM-30 feat: searilizers basic version done""
    This reverts commit c4a11e4762bd6a1f01a7667c5181772ea7c55676.

---
+++ backend/cryptomakler/app/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from models import User, Fund, Investment, Coin, Asset
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'role', 'email']
+
+
+class FundSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fund
+        fields = '__all__'
+
+
+class InvestmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Investment
+        fields = '__all__'
+
+
+class CoinSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coin
+        fields = '__all__'
+
+
+class AssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Asset
+        field = '__all__'
